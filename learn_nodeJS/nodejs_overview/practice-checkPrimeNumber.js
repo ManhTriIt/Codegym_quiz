d99f233fec11d7fd2a6145be39aa091f110f1fa6
ManhTriIt <mtri02.it@gmail.com>
2022-03-28 16:21:38 +0700

    feat: Add file pratice-checkPrimeNumber

---
+++ learn_nodeJS/nodejs_overview/practice-checkPrimeNumber.js
@@ -0,0 +1,19 @@
+const number = 2;
+let isPrimeNumber = true;
+
+if (number < 2) {
+  isPrimeNumber = false;
+}
+
+for (let index = 2; index < number - 1; index++) {
+  if (number % index === 0) {
+    isPrimeNumber = false;
+    break;
+  }
+}
+
+if (isPrimeNumber) {
+  console.log(`${number} là số nguyên tố`);
+} else {
+  console.log(`${number} không phải là số nguyên tố`);
+}
