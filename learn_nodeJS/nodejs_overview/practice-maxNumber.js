fe3163345f9b9c77df2c4bbe14e4f23a4f0099aa
ManhTriIt <mtri02.it@gmail.com>
2022-03-28 16:33:17 +0700

    feat: Add file practice-maxNumber

---
+++ learn_nodeJS/nodejs_overview/practice-maxNumber.js
@@ -0,0 +1,10 @@
+const array = [2, 5, 6, 456, 2, 76, 1000, 123, 888];
+let max = array[0];
+
+for (let number of array) {
+    if (max < number) {
+       max = number 
+    }
+}
+
+console.log(`Số lớn nhất là: ${max}`);
