f63b4d66eec55dba34e8b76b26207fad6af91b66
ManhTriIt <mtri02.it@gmail.com>
2022-03-28 16:53:45 +0700

    feat: Add file practice-averageScore

---
+++ learn_nodeJS/nodejs_overview/practice-averageScore.js
@@ -0,0 +1,26 @@
+const myClass = [
+  {
+    name: "An Nguyễn",
+    score: 9,
+  },
+  {
+    name: "Lê Phan",
+    score: 7,
+  },
+  {
+    name: "Bình Đỗ",
+    score: 10,
+  },
+  {
+    name: "Anh Phạm",
+    score: 8,
+  },
+];
+
+let totalScore = myClass.reduce((total, currentValue) => {
+    return currentValue.score+ total;
+}, 0)
+
+const average = totalScore / myClass.length;
+
+console.log(`Điểm trung bình của các học sinh trong lớp là: ${average}`);
