d57f51e7ae7db3570757c1f477d10765d3e8c987
ManhTriIt <mtri02.it@gmail.com>
2022-05-05 22:02:46 +0700

    feat: ejs template engine

--- learn-nodeJS/create-server-nodejs/index.js
+++ learn-nodeJS/create-server-nodejs/index.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/post", (req, res) => {
+  res.render("post");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
 
 app.listen(PORT, () => {
   console.log(`App listen on port ${PORT}`);
